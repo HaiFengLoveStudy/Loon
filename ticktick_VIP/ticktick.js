8d2becd513cdefbfb1eb189300de05382f374257
haifeng <chf3827@outlook.com>
2026-01-12 09:19:52 +0800

    change js

--- ticktick_VIP/ticktick.js
+++ ticktick_VIP/ticktick.js
@@ -1,16 +1,17 @@
+
 /*
-脚本功能：滴答清单解锁VIP (Loon版)
-更新时间：2026-01-12
-使用声明：仅供学习交流，请勿商用
+脚本功能：滴答清单解锁VIP
+适配工具：Loon
+文件位置：ticktick_VIP/ticktick.js
 */
 
 var body = $response.body;
 var obj = JSON.parse(body);
 
-// 修改 VIP 字段逻辑
-obj.proEndDate = "2099-01-01T00:00:00.000+0000";
-obj.needSubscribe = false;
-obj.pro = true;
+// 下面是原脚本的解混淆逻辑
+obj.proEndDate = "2099-01-01T00:00:00.000+0000"; // 到期时间
+obj.needSubscribe = false; // 是否需要订阅
+obj.pro = true; // 开启Pro状态
 
-// Loon 必须以对象形式输出 body
+// Loon 专用结束命令 (必须封装在对象里)
 $done({ body: JSON.stringify(obj) });
